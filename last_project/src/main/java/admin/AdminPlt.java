1fd05a4c5b5921c5650a080b84c7b6012a2ce69a
ss0fia <73986792+ss0fia@users.noreply.github.com>
2021-03-17 18:35:14 +0900

    예진- 시각화
    파이참 파일 따로 필요할듯...?

---
+++ last_project/src/main/java/admin/AdminPlt.java
@@ -0,0 +1,75 @@
+package main.java.admin;
+
+import org.python.util.PythonInterpreter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+/*public class admin_what{
+
+    String cmd = "D:\\blue\\last_project\\WebContent";
+    String py = "admin_plt";
+    String run = "python  " +cmd+ py + ".py";
+    System.out.println(run);
+    //Runtime.getRuntime().exec(run);
+
+    Process p = Runtime.getRuntime().exec("python  D:\\blue\\last_project\\WebContent admin_plt.py");
+    
+}*/
+
+/*
+ * @Controller public class AdminPlt {
+ * 
+ * private static PythonInterpreter interpreter;
+ * 
+ * @RequestMapping(value="admin/admin_plt.do") public static String
+ * main(String[] args) {
+ * 
+ * interpreter = new PythonInterpreter(); System.out.println("1");
+ * interpreter.execfile("admin_plt.py"); System.out.println("2");
+ * interpreter.exec("da"); System.out.println("3");
+ * 
+ * return "admin/admin_plt"; }
+ * 
+ * }
+ */
+
+@Controller
+public class AdminPlt{
+	
+	@GetMapping("admin/admin_what.do")
+	public ModelAndView viewChart() {
+	     ModelAndView mv = new ModelAndView("admin/admin_what");
+	     final String uri = "http://localhost:5000/getBarChartBase64";
+	     RestTemplate restTemplate = new RestTemplate();
+	     String base64Img = restTemplate.getForObject(uri, String.class);
+	     mv.addObject("base64Img", base64Img);
+	     return mv;
+	}
+	
+	
+	@GetMapping("admin/admin_what2.do")
+	public ModelAndView viewChart2() {
+	     ModelAndView mv = new ModelAndView("admin/admin_what2");
+	     final String uri = "http://localhost:5000/getPieChartBase64";
+	     final String uri2 = "http://localhost:5000/getBarChartBase64_2";
+	     RestTemplate restTemplate = new RestTemplate();
+	     String base64Img = restTemplate.getForObject(uri, String.class);
+	     String base64Img_2 = restTemplate.getForObject(uri2, String.class);
+	     mv.addObject("base64Img", base64Img);
+	     mv.addObject("base64Img_2", base64Img_2);
+	     return mv;
+	}
+	
+	/*
+	 * @GetMapping("admin/admin_what2.do") public ModelAndView viewChart3() {
+	 * ModelAndView mv = new ModelAndView("admin/admin_what2"); final String uri =
+	 * "http://localhost:5000/getBarChartBase64_2"; RestTemplate restTemplate = new
+	 * RestTemplate(); String base64Img = restTemplate.getForObject(uri,
+	 * String.class); mv.addObject("base64Img_2", base64Img); return mv; }
+	 */
+
+
+}
